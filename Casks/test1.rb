4a726d0bbb54ccc91d30f0ed6d75ff7d8ba41900
Alister718 <Alister718@gmail.com>
2018-04-03 16:50:59 -0400

    Update and rename testdiscord.rb to test1.rb

--- Casks/test1.rb
+++ Casks/test1.rb
@@ -1,11 +1,11 @@
-cask 'testdiscord' do
+cask 'test1' do
   version '0.0.250'
   sha256 :no_check
 
   url "https://cdn.discordapp.com/apps/osx/#{version}/Discord.dmg"
   appcast 'https://discordapp.com/api/stable/updates?platform=osx',
           checkpoint: '0bca6e26a844f761d56ba49d709170a02930f24942809728452e6a8179a94c07'
-  name 'testdiscord'
+  name 'test1'
   homepage 'https://discordapp.com/'
 
   auto_updates true
