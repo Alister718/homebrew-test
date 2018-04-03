7cabce089ca911a8d2bf18852d532531265e2092
Alister718 <Alister718@gmail.com>
2018-04-03 13:39:20 -0400

    Create virtualbox.rb

---
+++ Casks/virtualbox.rb
@@ -0,0 +1,9 @@
+cask 'virtualbox' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://download.virtualbox.org/virtualbox/5.2.8/VirtualBox-5.2.8-121009-OSX.dmg'
+  name 'VirtualBox'
+  homepage 'https://www.virtualbox.org/'
+  
+end
