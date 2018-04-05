c98ca0b48b93c925da50deda33f42622e7b9c88d
Alister718 <Alister718@gmail.com>
2018-04-05 13:25:48 -0400

    Update test2.rb

--- Casks/test2.rb
+++ Casks/test2.rb
@@ -5,8 +5,7 @@ cask 'test2' do
   url 'http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jre-8u161-macosx-x64.dmg'
   name 'test2'
   homepage 'https://www.oracle.org/'
-  
-  dmg 'jre-8u161-macosx-x64.dmg'
+ 
   pkg 'Java 8 Update 161.pkg'
   
 end
