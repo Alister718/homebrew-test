cd4c9189cd2e2bb4f3af10b9b32c7614bd0e92fb
Alister718 <Alister718@gmail.com>
2018-04-05 13:08:07 -0400

    Update firefox.rb

--- Casks/firefox.rb
+++ Casks/firefox.rb
@@ -1,11 +1,9 @@
-cask 'firefox' do
+cask 'Java8_161' do
   version :latest
   sha256 :no_check
 
-  url 'https://download.mozilla.org/?product=firefox-latest-ssl&os=osx&lang=en-US'
-  name 'firefox'
-  homepage 'https://www.firefox.org/'
-  
-  app 'Firefox.app'
+  url 'http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jre-8u161-macosx-x64.dmg'
+  name 'Java8_161'
+  homepage 'https://www.oracle.org/'
   
 end
