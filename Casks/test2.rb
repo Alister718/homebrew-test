48ae71bc48407ff88100494517fd224bf2fef9fe
Alister718 <Alister718@gmail.com>
2018-04-05 13:11:56 -0400

    Update and rename Java8.rb to test2.rb

--- Casks/test2.rb
+++ Casks/test2.rb
@@ -1,9 +1,9 @@
-cask 'Java8' do
+cask 'test2' do
   version :latest
   sha256 :no_check
 
   url 'http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jre-8u161-macosx-x64.dmg'
-  name 'Java8'
+  name 'test2'
   homepage 'https://www.oracle.org/'
   
 end
