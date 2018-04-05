82fd991f0a7b78f4acc1c3de12e54d12675633b2
Alister718 <Alister718@gmail.com>
2018-04-05 13:51:20 -0400

    Update test2.rb

--- Casks/test2.rb
+++ Casks/test2.rb
@@ -2,10 +2,13 @@ cask 'test2' do
   version :latest
   sha256 :no_check
 
-  url 'http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jre-8u161-macosx-x64.dmg'
-  name 'test2'
+  url 'http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-macosx-x64.dmg'
+     cookies: {
+                 'oraclelicense' => 'accept-securebackup-cookie',
+               }
+  name 'Java Standard Edition Development Kit'  
   homepage 'https://www.oracle.org/'
  
-  dmg 'test2--latest.dmg'
+  pkg 'JDK 8 Update 161.pkg'
   
 end
